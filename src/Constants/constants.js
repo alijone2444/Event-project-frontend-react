c39f18f1944b28fd9611c99be1ab0047cc9613f2
alijone2444 <alijone2444@gmail.ccom>
2024-03-13 14:59:28 +0500

    changing url

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -5,12 +5,12 @@ import societyicon from '../images/societyicon.png'
 import CalendarTodayOutlined from '@mui/icons-material/CalendarTodayOutlined';
 const constants = {
 
-menuitems:[
-    {name:"Home",icon:<HomeOutlined/>},
-    {name:"Events",icon:<StarBorderOutlined/>},
-    {name:"Societies",icon:<img  alt="Event Icon" src={societyicon} style={{width:"24px",height:"24px"}} />},
-    {name:"Calander",icon:<CalendarTodayOutlined/>},
-],
-BASE_URL :'http://localhost:3002/',
+    menuitems: [
+        { name: "Home", icon: <HomeOutlined /> },
+        { name: "Events", icon: <StarBorderOutlined /> },
+        { name: "Societies", icon: <img alt="Event Icon" src={societyicon} style={{ width: "24px", height: "24px" }} /> },
+        { name: "Calander", icon: <CalendarTodayOutlined /> },
+    ],
+    BASE_URL: 'http://192.168.1.11:3002',
 }
 export default constants;

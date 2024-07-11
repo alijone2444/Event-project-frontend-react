4e4ab7ede45669dfdd3bc0d51b7171ffdc36211f
alijone2444 <alijone2444@gmail.ccom>
2024-07-11 23:05:44 +0500

    link change

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -25,7 +25,9 @@ const constants = {
     // BASE_URL: 'http://192.168.1.15:3002/',
     // BASE_URL_2: 'http://192.168.1.15:5000/',
 
-    BASE_URL: 'http://192.168.1.9:3002/',
+    // BASE_URL: 'http://192.168.1.9:3002/',
+    BASE_URL: 'https://ec2e-182-180-46-244.ngrok-free.app/',
+
     BASE_URL_2: 'http://192.168.60.129:5000/',
     departmentOptions: [
         "Aeronautics and Astronautics",

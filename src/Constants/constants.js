ea21f96774ddbc491f32cf7d59bbda3f58fbe812
alijone2444 <alijone2444@gmail.ccom>
2024-07-12 15:55:20 +0500

    .

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -26,7 +26,7 @@ const constants = {
     // BASE_URL_2: 'http://192.168.1.15:5000/',
 
     // BASE_URL: 'http://192.168.1.9:3002/',
-    BASE_URL: 'https://4993-182-180-46-244.ngrok-free.app/',
+    BASE_URL: 'https://53d5-182-180-46-244.ngrok-free.app/',
 
     BASE_URL_2: 'http://192.168.60.129:5000/',
     departmentOptions: [
